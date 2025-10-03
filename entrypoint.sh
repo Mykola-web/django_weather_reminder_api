d32665e237c62116228f95423d6b01dbb84c92ca
Mykola Karandiei <korondei@gmail.com>
2025-10-03 04:25:19 +0300

    changed entrypoint.sh to run fake_data_maker command

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -e  # exit immediately if a command fails
 echo "Running migrations..."
 python manage.py migrate
 
+#makin 5 test users with 1 subscription each
+python manage.py fake_data_maker
+
 # Optional: create superuser from environment variables
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
     echo "Creating superuser..."
