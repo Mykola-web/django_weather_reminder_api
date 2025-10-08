a4d1b4bd958146e57e2c2ad53c6a6174bf5d534c
Mykola Karandiei <korondei@gmail.com>
2025-10-08 04:17:18 +0300

    celery_real

--- api/celery.py
+++ api/celery.py
@@ -1,19 +1,19 @@
-# import os
-# from celery import Celery
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_project.settings')
-#
-# app = Celery('weather_project')
-#
-# app.config_from_object('django.conf:settings', namespace='CELERY')
-#
-# # connect to redis.io free db
-# app.conf.broker_url = (
-#     os.environ.get('REDIS_URL', 'redis://redis-15101.c328.europe-west3-1.gce.redns.redis-cloud.com:15101/0'))
-# app.conf.result_backend = (
-#     os.environ.get('REDIS_URL', 'redis://redis-15101.c328.europe-west3-1.gce.redns.redis-cloud.com:15101/0'))
-# app.conf.task_serializer = 'json'
-# app.conf.result_serializer = 'json'
-# app.conf.accept_content = ['json']
-#
-# app.autodiscover_tasks()
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_project.settings')
+
+app = Celery('weather_project')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# connect to redis.io free db
+app.conf.broker_url = (
+    os.environ.get('REDIS_URL', 'redis://redis-15101.c328.europe-west3-1.gce.redns.redis-cloud.com:15101/0'))
+app.conf.result_backend = (
+    os.environ.get('REDIS_URL', 'redis://redis-15101.c328.europe-west3-1.gce.redns.redis-cloud.com:15101/0'))
+app.conf.task_serializer = 'json'
+app.conf.result_serializer = 'json'
+app.conf.accept_content = ['json']
+
+app.autodiscover_tasks()
